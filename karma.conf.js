9edeb489bd626d249ef245b10b17be160b4fc2e0
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-22 11:50:03 -0700

    Use versal-gadget-api

--- karma.conf.js
+++ karma.conf.js
@@ -2,9 +2,8 @@ module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'chai'],
     files: [
-      'bower_components/versal-component-runtime/dist/runtime.min.js',
       'bower_components/eventEmitter/EventEmitter.js',
-      'bower_components/versal-player-api/index.js',
+      'bower_components/versal-gadget-api/versal-player-api.js',
       'bower_components/jquery/dist/jquery.js',
       'bower_components/jquery-autosize/jquery.autosize.js',
       'bower_components/highlightjs/highlight.pack.js',
